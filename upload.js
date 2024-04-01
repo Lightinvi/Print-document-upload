e0a01ff609391553900eb6142995ec61c5d64854
Lightinvi <59226491+Lightinvi@users.noreply.github.com>
2024-04-01 11:10:38 +0800

    Update upload.js

--- upload.js
+++ upload.js
@@ -19,9 +19,10 @@ document.addEventListener('DOMContentLoaded', function(){
             formData.append('file', file);
             fetch('https://discord.com/api/v10/channels/1146358218696691764/messages', {
                 method: 'POST',
+                credentials: 'include',
                 body: formData,
                 headers: {
-                    'Content-Type': 'application/json',
+                    'multipart/form-data',
                     'accept': "/",
                     'authority': "discordapp.com",
                     'Authorization': decrypt("uY3ANQt+2c5P7zNDzoUvf3g5+HFOHsT6M2Rtrc3553eZKH5UCc50oxJxrHmptNaegb2TWzhlKySHnP6bJJWalfu6du4DKmdxgtsvMmokxXo="),
